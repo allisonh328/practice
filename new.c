1362abbc5ff8b511dc9702f2db883e81d836459e
Allison <allisonh.328@gmail.com>
2021-01-20 15:48:11 +0000

    smile!

--- new.c
+++ new.c
@@ -3,6 +3,13 @@
 int main(void) {
   puts("Hello Evy!");
   puts("It's a beautiful day!");
+  puts("   00        00   ");
+  puts("   00        00   ");
+  puts("                  ");
+  puts("000000000000000000");
+  puts(" 00            00 ");
+  puts("  000        000  ");
+  puts("   000000000000   ");
   return 0;
 
 }
