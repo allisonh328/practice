5e0df170ed3abd7f5a14553a441972f7ac28c191
Allison <allisonh.328@gmail.com>
2021-01-25 16:09:15 +0000

    added hug capability

--- hello.c
+++ hello.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 
+void give_hug();
+
 int main(void)
 {
   puts("Why, hello there, world!");
   puts("It's so lovely to see you!");
+  give_hug();
   return 0;
+}
+
+void give_hug() 
+{
+  puts("  000000     000000  ");
+  puts("0000000000 0000000000");
+  puts("000000000000000000000");
+  puts("000000000000000000000");
+  puts(" 0000000000000000000 ");
+  puts("    0000000000000    ");
+  puts("       0000000       ");
+  puts("         000         ");
+  puts("          0          ");
 }
